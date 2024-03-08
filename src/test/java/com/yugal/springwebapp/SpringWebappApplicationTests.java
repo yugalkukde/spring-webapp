d52a507813a94fdd20c39d49fd594822f5a7acdc
u=yugalkukde <yugalkukde7@gmail.com>
2024-03-08 08:16:14 +0530

    initial commit upload

---
+++ src/test/java/com/yugal/springwebapp/SpringWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yugal.springwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
