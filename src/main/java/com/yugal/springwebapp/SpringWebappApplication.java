6ae1b15d49a86596df4a351ef6f0b59f33199dd7
u=yugalkukde <yugalkukde7@gmail.com>
2024-03-10 14:20:12 +0530

    first part, added index html for a static html page. Added dependencies thymeleaf and devtools

--- src/main/java/com/yugal/springwebapp/SpringWebappApplication.java
+++ src/main/java/com/yugal/springwebapp/SpringWebappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringWebappApplication {
 
 	public static void main(String[] args) {
+        //System.out.println("test");
 		SpringApplication.run(SpringWebappApplication.class, args);
 	}
 
